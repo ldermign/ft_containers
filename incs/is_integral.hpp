6ea4c2ee5958a05785c4d44649e7173385181b16
Liena DERMIGNY <ldermign@studentx.42.fr>
2022-10-19 10:48:59 +0200

    is_integral ok, check call function

--- incs/is_integral.hpp
+++ incs/is_integral.hpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 15:40:01 by ldermign          #+#    #+#             */
-/*   Updated: 2022/10/18 15:48:36 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/10/19 10:48:36 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,56 @@
 
 START
 
-template<class T, T v >
-struct integral_constant {
-	static constexpr T value = v;
-	using value_type = T;
-	using type = integral_constant; // using injected-class-name
-	constexpr operator value_type() const noexcept { return value; }
+template< class T, T v >
+struct integral_const {
+
+	static const T value = v;
+	typedef T value_type;
+	typedef integral_const< T, v > type; // using injected-class-name
+	// operator T() {
+	// 	return v;
+	// }
+	operator value_type() const {
+		return value;
+	}
 };
 
+// template < class T >
+//    struct is_integral : public integral_constant< bool, is_integral(T) > {};
+
+typedef integral_const< bool, true > true_type;
+typedef integral_const< bool, false > false_type;
+
+template < class T > struct is_integral : public false_type {};
+// template < class T > struct is_integral : public std::true_type {};
+template<> struct is_integral< unsigned char > : public true_type {};
+template<> struct is_integral< unsigned short > : public true_type{};
+template<> struct is_integral< unsigned int > : public true_type{};
+template<> struct is_integral< unsigned long > : public true_type{};
+template<> struct is_integral< signed char > : public true_type{};
+template<> struct is_integral< short > : public true_type{};
+template<> struct is_integral< int > : public true_type{};
+template<> struct is_integral< long > : public true_type{};
+template<> struct is_integral< char > : public true_type{};
+template<> struct is_integral< bool > : public true_type{};
+
 STOP
+
+/* INFO
+
+integral_const template :
+https://cplusplus.com/reference/type_traits/integral_constant/
+mix avec 
+https://en.cppreference.com/w/cpp/types/integral_constant
+(beaucoup moins le deuxieme, erreurs avec using)
+
+template pour integral :
+https://code.woboq.org/appleseed/include/boost/type_traits/is_integral.hpp.html
+
+true and false type :
+https://cplusplus.com/reference/type_traits/false_type/
+https://cplusplus.com/reference/type_traits/true_type/
+
+*/
 
 #endif
